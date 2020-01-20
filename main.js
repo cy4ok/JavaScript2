e44f9b9a15e1cb1003e346bead7f1d6f9cd5cdc0
cy4ok <cy4ok86@gmail.com>
2020-01-20 15:57:06 +0300

    Забыл сократить функцию. Исправил.

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ const goods = [
     { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
 ];
 
-const renderGoodsItem = (title = '', price = '', img = '') => {
-    return `<div class="goods-item">
+const renderGoodsItem = (title = '', price = '', img = '') =>
+    `<div class="goods-item">
         <img src="${img}" alt="alt">
         <h3 class="title-goods-item">${title}</h3>
         <p>${price}</p>
         <button class="buy-item">Купить</button>
-    </div>`
-};
+    </div>`;
 
 const renderGoodsList = (list = [], container = '') => {
     const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img));
